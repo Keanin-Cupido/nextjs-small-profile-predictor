1b8e5e3b090bf7389c4137412ae4b225a0db3f9e
Keanin Cupido <keanin16@gmail.com>
2024-04-05 21:07:12 +0200

    feat: add fetch data

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,10 +1,40 @@
+const getPredictedAge = async (name: string) => {
+    const res = await fetch(`https://api.agify.io/?name=${name}`);
+    return res.json();
+}
+const getPredictedGender = async (name: string) => {
+    const res = await fetch(`https://api.genderize.io/?name=${name}`);
+    return res.json();
+}
+const getPredictedCountry = async (name: string) => {
+    const res = await fetch(`https://api.nationalize.io/?name=${name}`);
+    return res.json();
+}
+
 interface Params {
     params: { name: string }
 }
 
-export default function Prediction({params}: Params) {
+export default async function Prediction({params}: Params) {
+    const ageData = getPredictedAge(params.name);
+    const genderData = getPredictedGender(params.name);
+    const countryData = getPredictedCountry(params.name);
+
+    const [age, gender, country] = await Promise.all([
+        ageData,
+        genderData,
+        countryData
+    ]);
+
     const label = "hello"
     return (
-        <h1>{params.name}</h1>
+        <div>
+            <div>
+                <div>Personal Info</div>
+                <div>Age: {age?.age}</div>
+                <div>Gender: {gender?.gender}</div>
+                <div>Country: {country?.country[0]?.country_id}</div>
+            </div>
+        </div>
     )
 }
