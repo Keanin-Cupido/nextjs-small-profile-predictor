a26ede8071ede73496c73541973ede2dde4837eb
Keanin Cupido <keanin16@gmail.com>
2024-04-06 15:08:13 +0200

    add better home page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,17 +14,17 @@ export default function Home() {
   }
 
   return (
-    <main>
+    <main className="w-full min-h-screen flex flex-col items-center justify-center gap-8">
       <div>
-        <h1>Enter Your Name</h1>
+        <h1 className="text-center text-3xl text-yellow-400">Enter Your Name</h1>
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center gap-8">
         <input type="text" name="" id="" placeholder="Enter your name..." 
           value={inputVal}
-          className="text-cyan-900"
+          className="text-yellow-400 p-2 pl-4 text-lg rounded-lg outline-none bg-zinc-950 focus:bg-zinc-900"
           onChange={(e) => setInputVal(e.target.value)}
         />
-        <button type="submit">Predict Data</button>
+        <button type="submit" className="px-6 py-4 text-black rounded-lg cursor-pointer bg-yellow-400 hover:bg-yellow-500">Predict Data</button>
       </form>
     </main>
   );
