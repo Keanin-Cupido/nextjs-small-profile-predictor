c401fc342ab682a2469f1c0f5acf9d85e43a8624
Keanin Cupido <keanin16@gmail.com>
2024-04-06 15:17:27 +0200

    add better prediction page UI

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const getPredictedAge = async (name: string) => {
     const res = await fetch(`https://api.agify.io/?name=${name}`);
@@ -29,14 +30,21 @@ export default async function Prediction({params}: Params) {
     ]);
 
     return (
-        <div>
-            <div>
-                <div>Personal Info</div>
+        <div className="w-full min-h-screen flex flex-col items-center justify-center gap-8">
+            <div className="flex flex-col items-center justify-center gap-2">
+                <div className="text-center text-3xl text-yellow-400 mb-4">Personal Info</div>
 
-                <img src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${params.name}`} className="w-[64px] h-[64px]" alt="avatar" />
-                <div>Age: {age?.age}</div>
-                <div>Gender: {gender?.gender}</div>
-                <div>Country: {country?.country[0]?.country_id}</div>
+                <img src={`https://api.dicebear.com/8.x/pixel-art/svg?seed=${params.name}`} alt="avatar" 
+                    className="w-[96px] h-[96px] rounded-lg border border-yellow-400 bg-yellow-300 mb-4" 
+                />
+                <div className="text-yellow-400"><b>Name:</b> {params.name}</div>
+                <div className="text-yellow-400"><b>Age:</b> {age?.age}</div>
+                <div className="text-yellow-400"><b>Gender:</b> {gender?.gender}</div>
+                <div className="text-yellow-400"><b>Country:</b> {country?.country[0]?.country_id}</div>
+
+                <Link href="/">
+                    <button className="mt-4 px-6 py-2 text-black rounded-lg cursor-pointer bg-yellow-400 hover:bg-yellow-500">Back</button>
+                </Link>
             </div>
         </div>
     )
